759f377f977d1fb305d5054ee3d7c0e42f63666c
Theme Frugal <arunram.atmacharan@iiml.org>
2022-05-26 21:16:13 +0530

    Added Net Profit (need to be corrected for schemes which have not been redeemed fully)

--- app.py
+++ app.py
@@ -8,15 +8,18 @@ from io import StringIO
 # Source: https://github.com/codereverser/casparser
 
 @st.cache
-def get_data():
+def get_data(file_path):
     data_csv = casparser.read_cas_pdf(file_path, "", output='csv')
     df_data = pd.read_csv(StringIO(data_csv))
     return df_data
 
-df_data = get_data()
+df_full = get_data(file_path)
+df_data = df_full.copy()
 df_data['folio_scheme'] = df_data['folio'].astype(str) + ' ' + df_data['scheme']
 
 sel_folio = st.sidebar.selectbox("Select Folio:", df_data['folio_scheme'].unique())
 df_folio = df_data[df_data['folio_scheme'] == sel_folio]
 
 st.write(df_folio)
+st.write('Net Profit:')
+st.write(-df_folio.amount.sum().round(2))
