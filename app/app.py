ddb81e85648166dc6a1e7f9a7b429758c6ec9a4e
Theme Frugal <arunram.atmacharan@iiml.org>
2023-02-15 21:36:13 +0530

    cas analysis to do items

--- app/app.py
+++ app/app.py
@@ -3,3 +3,5 @@ import numpy as np
 import streamlit as st
 
 # To use cas_parser library directly from this application.
+# Any date to any date processing
+# capital gains statements - and scenario analysis
