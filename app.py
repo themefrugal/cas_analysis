3177ee12a843fbf876023a600374f2e5dd380507
Theme Frugal <arunram.atmacharan@iiml.org>
2023-05-12 23:57:15 +0530

    minor changes

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from io import StringIO
 # Base file to create a streamlit application using cas_parser
 # Source: https://github.com/codereverser/casparser
 # To do: Also need to compare with any MF using mfapi.in
+# Make a finance and investment utils library
 
 @st.cache_data
 def get_data(file_path):
--- app/app.py
+++ app/app.py
@@ -5,3 +5,4 @@ import streamlit as st
 # To use cas_parser library directly from this application.
 # Any date to any date processing
 # capital gains statements - and scenario analysis
+
