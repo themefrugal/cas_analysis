0da8185fcc0d8b23d5c1cddcfd01711d48c4a159
Theme Frugal <arunram.atmacharan@iiml.org>
2024-07-17 23:59:11 +0530

    Updated regex based on the new CAS template

--- app/cas_regex.R
+++ app/cas_regex.R
@@ -1,5 +1,5 @@
 closing_line_pattern <-
-    'Closing\\s+Unit\\s+Balance:\\s+([0-9,.]+).*INR\\s+([0-9,.]+).*Valuation\\s+on\\s+(\\d{2}-[A-Za-z]{3}-\\d{4}):\\s+INR\\s+([0-9,.]+)'
+    'Closing\\s+Unit\\s+Balance:\\s+([0-9,.]+).*INR\\s+([0-9,.]+).*Market\\s+Value\\s+on\\s+(\\d{2}-[A-Za-z]{3}-\\d{4}):\\s+INR\\s+([0-9,.]+)'
 fund_name_pattern <- "^[A-Z0-9]+-[A-Za-z&]+\\s"
 fund_advisor_pattern <- '(.+)\\(Advisor:\\s+(.*)\\)'
 transaction_pattern <- '(\\d{2}-[A-Za-z]{3}-\\d{4})(.*)((\\s+[(),.0-9]+){4})'
